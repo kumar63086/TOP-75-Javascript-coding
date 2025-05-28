83402a33855bcf769ff34215be50d6684d177b3d
kumar866 <kumarborelli0@gmail.com>
2025-05-28 13:33:06 +0530

    check the ingers

--- 11.checktheintgers.js
+++ 11.checktheintgers.js
@@ -1,4 +1,10 @@
 const num = 10;
 console.log(Number.isInteger(num)); // Output: true
 console.log(Number.isInteger(10.5)); // Output: false
-console.log(Number.isInteger("10")); // Output: false   
+console.log(Number.isInteger("10")); // Output: false
+
+if(num%2===0){
+    console.log("it is Intergres");
+} else {
+    console.log("!not Inters");
+}
