a63ec28e02990a7b7457f6f643c125ec330a273a
kumar866 <kumarborelli0@gmail.com>
2025-05-28 13:38:31 +0530

    create the duplicate arrayvalues

--- 11.checktheintgers.js
+++ 11.checktheintgers.js
@@ -1,4 +1,4 @@
-const num = 10;
+const num = 10.5;
 console.log(Number.isInteger(num)); // Output: true
 console.log(Number.isInteger(10.5)); // Output: false
 console.log(Number.isInteger("10")); // Output: false
---
+++ 12.duplicatevalues.js
@@ -0,0 +1,6 @@
+const arr = [1,2,3,4,5,6,7,8,9,10];
+function Dupicate(arr){
+    return arr.concat(arr);
+}
+
+console.log(Dupicate(arr));
